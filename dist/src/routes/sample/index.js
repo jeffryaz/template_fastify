025dd1d40eb003b019822b0befc0981e5b9f3c8a
Jeffry Azhari Rosman <jeffryazhari@gmail.com>
2023-04-20 12:32:29 +0700

    delete folder dist and log

--- dist/src/routes/sample/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const controllers_1 = require("../../controllers");
-const routerSample = (fastify, opts, done) => {
-    fastify.get('/', controllers_1.sample);
-    done();
-};
-exports.default = routerSample;
